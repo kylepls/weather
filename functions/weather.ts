057ee4bd120d2f41030f501933a23309f286a1a7
kylepls <mail@kyle.in>
2019-09-15 14:02:19 -0600

    Merge client & server

---
+++ functions/weather.ts
@@ -0,0 +1,28 @@
+import {APIGatewayEvent} from "aws-lambda";
+import {Coordinates} from "./earthquakes";
+
+const DarkSky = require('dark-sky');
+const key = process.env.DARK_SKY;
+if (!key) {
+    throw Error("No DARK_SKY key found")
+}
+const darksky = new DarkSky(key);
+
+export async function handler(event: APIGatewayEvent): Promise<any> {
+    const body = JSON.parse(event.body || "");
+    const coords: Coordinates = body as Coordinates;
+    const data = await getData(coords);
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data)
+    }
+}
+
+async function getData(coords: Coordinates): Promise<any> {
+    return new Promise((resolve, reject) => {
+        darksky.coordinates({
+            lat: coords.latitude,
+            lng: coords.longitude
+        }).get().then(resolve).catch(reject);
+    });
+}
