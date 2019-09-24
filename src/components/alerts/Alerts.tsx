1a42c609f5a15763ef7ac40c98cda59365358aff
kylepls <mail@kyle.in>
2019-09-24 10:46:53 -0600

    Added alerts implementation #17

--- src/components/alerts/Alerts.tsx
+++ src/components/alerts/Alerts.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {AppContext} from 'App';
 
-export function Alerts({alerts}) {
-  return (
-    <div className="alerts">
-      {alerts.map((a, i) => (<Alert key={i} alert={a}/>))}
-    </div>
-  );
+type Severity = 'advisory' | 'watch' | 'warning';
+
+interface WeatherAlert {
+  title: string;
+  time: Date;
+  expires: Date;
+  severity: Severity;
+  description: string;
+  uri: string;
 }
 
-function Alert({alert}) {
+export function Alerts() {
+  const {weather, weatherError} = useContext(AppContext);
+  if (weatherError || !weather || !weather.alerts) {
+    return (<></>);
+  } else {
+    const alerts: WeatherAlert[] = weather.alerts.map((a: any) => {
+      return {
+        title: a.title,
+        time: new Date(a.time * 1000),
+        expires: new Date(a.expires * 1000),
+        description: a.description,
+      };
+    });
+    return (
+      <div className="alerts">
+        {alerts.map((a, i) => (<Alert key={i} alert={a}/>))}
+      </div>
+    );
+  }
+}
+
+function Alert({alert}: { alert: WeatherAlert }) {
   return (
     <div className="alert">
       {alert.severity}: {alert.title}
