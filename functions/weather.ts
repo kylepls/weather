27d775235a73b0e674fa2640a824a0e9e3204949
kylepls <mail@kyle.in>
2019-09-24 11:18:57 -0600

    Added simple rate limiting fix #16

--- functions/weather.ts
+++ functions/weather.ts
@@ -5,13 +5,31 @@ const DarkSky = require('dark-sky');
 require('dotenv').config();
 
 const key = process.env.DARK_SKY;
+const dev = process.env.DEV;
 if (!key) {
   throw Error('No DARK_SKY key found');
 }
 
+const limiter = require('lambda-rate-limiter')({
+  interval: 60 * 60 * 1000,
+  uniqueTokenPerInterval: 500,
+});
+
 const darksky = new DarkSky(key);
 
 export async function handler(event: APIGatewayEvent): Promise<any> {
+  if (!dev) {
+    const ip = getSourceIp(event.requestContext);
+
+    try {
+      await limiter.check(20, ip);
+    } catch (e) {
+      return {
+        statusCode: 429,
+      };
+    }
+  }
+
   const body = JSON.parse(event.body || '');
   const coords: Coordinates = body as Coordinates;
   const data = await getData(coords);
@@ -28,4 +46,12 @@ async function getData(coords: Coordinates): Promise<any> {
       lng: coords.longitude,
     }).get().then(resolve).catch(reject);
   });
+}
+
+function getSourceIp(requestContext) {
+  if (requestContext) {
+    return requestContext.identity.sourceIp;
+  } else {
+    throw Error('Could not get source IP');
+  }
 }
