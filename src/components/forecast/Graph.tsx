a79de4b0dcc943c39b36f70377bdba6ac962322a
Joshua <joshuabetz12@gmail.com>
2020-06-11 00:38:23 -0400

    updated look to match current one

--- src/components/forecast/Graph.tsx
+++ src/components/forecast/Graph.tsx
@@ -1,14 +1,19 @@
 import React, {useContext} from 'react';
-import moment from 'moment';
+import moment, {Moment} from 'moment';
 import {AppContext} from 'App';
-import {Area, AreaChart, ResponsiveContainer, XAxis, YAxis} from 'recharts';
+import {Area, AreaChart, LabelList, ResponsiveContainer, XAxis, YAxis} from 'recharts';
 import './Forecast.css';
 
 
+function getApplicableDataPoints(data) {
+  const now = moment();
+  const cutoff = moment().add(12, 'hour');
+  const within = (time: Moment) => time.isAfter(now) && time.isBefore(cutoff);
+  return data.filter((element) => within(moment.unix(element.time)));
+}
+
 function createRechartData(data) {
-  const cutoff = moment().subtract(16, 'hours');
-  return data.filter((element) => moment.unix(element.time).isAfter(cutoff))
-      .map((element) => createRechartDataPoint(element));
+  return getApplicableDataPoints(data).map((element) => createRechartDataPoint(element));
 }
 
 function createRechartDataPoint(weatherDataPoint) {
@@ -18,8 +23,18 @@ function createRechartDataPoint(weatherDataPoint) {
   };
 }
 
-function withDegree(temp) {
-  return temp + '˚';
+function makeTextLabel(props) {
+  const {x, y, value} = props;
+  return (
+    <g>
+      <text fontSize='larger' x={x} y={y - 30} fill='#ffffff'>{value}</text>
+    </g>
+  );
+}
+
+function getMinMaxTemperature(data) {
+  const temperatures = data.map((point) => point.temperature);
+  return [Math.min(...temperatures), Math.max(...temperatures)];
 }
 
 export default function Graph() {
@@ -31,19 +46,37 @@ export default function Graph() {
   }
 
   const hourly = weather.hourly.data;
+  const [min, max] = getMinMaxTemperature(getApplicableDataPoints(hourly));
+
+  const getLowerBoundTemp = () => {
+    return Math.floor(min - 1);
+  };
+
+  const getUpperBoundTemp = () => {
+    return Math.ceil(max + 1);
+  };
 
   return (
-    <ResponsiveContainer className='forecastGraph' maxHeight='50%'>
-      <AreaChart margin={{top: 15, right: 30, left: 20, bottom: 5}} data={createRechartData(hourly)}>
-        <defs>
-          <linearGradient id="colorTemp" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#ffffff" stopOpacity={0.8}/>
-            <stop offset="95%" stopColor="#ffffff" stopOpacity={0.15}/>
-          </linearGradient>
-        </defs>
-        <XAxis interval={5} tickFormatter={(time) => moment.unix(time).format('hh:mm a')} dataKey='time'/>
-        <YAxis tickFormatter={(temp) => withDegree(temp)} dataKey='temp' />
-        <Area type='monotone' dataKey='temp' stroke='#ffffff' fillOpacity={1} fill="url(#colorTemp)"/>
+    <ResponsiveContainer className='forecastGraph' height='50%'>
+      <AreaChart margin={{top: 15, right: 20, left: 20, bottom: 5}} data={createRechartData(hourly)}>
+        <XAxis
+          axisLine={false}
+          tickMargin={15}
+          interval={1}
+          minTickGap={2}
+          stroke='#ffffff'
+          tick={{fontSize: 20}}
+          tickSize={0}
+          tickFormatter={(time) => moment.unix(time).format('h A')}
+          dataKey='time' />
+        <YAxis
+          allowDataOverflow={true}
+          hide={true}
+          domain={[getLowerBoundTemp(), getUpperBoundTemp()]}
+          dataKey='temp' />
+        <Area type='monotone' dataKey='temp' strokeWidth={4} stroke='#324ea8' fillOpacity={0.25} fill='#0048ff'>
+          <LabelList content={makeTextLabel} dataKey='temp' />
+        </Area>
       </AreaChart>
     </ResponsiveContainer>
   );
